d96b9a7433d653de9eaa947e6e6e740715d012be
arnsteio <arnstein.orten@gmail.com>
2018-04-04 09:43:57 +0200

    Added build version numbering

--- bin/onPush.sh
+++ bin/onPush.sh
@@ -3,10 +3,12 @@
 # It is meant to be invoked from git hooks (.git/hooks/)
 
 BUILDDIR=~/git/ull
+VER=ull_v4
 
 touch /tmp/onPushStart
 cd $BUILDDIR
 git pull
-make *.stl
+rm $VER.stl
+make $VER.stl
 touch /tmp/onPushEnd
 
